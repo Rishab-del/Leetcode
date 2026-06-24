1b498b047bb1861ae398779e3f3b16c53b03f357
Rishabh patel <rishabh24464@iiitd.ac.in>
2026-06-24 23:02:59 +0530

    Time: 0 ms (100%), Space: 17.6 MB (51.77%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -18,17 +18,22 @@ public:
     //     preorder(root->right,ans);
     // }
     void flatten(TreeNode* root) {
-        if(root == NULL) return;
-        TreeNode* l =root->left;
-        TreeNode* r = root -> right;
-        flatten(l);
-        flatten(r);
-        root->left = NULL;
-        root->right = l;
-        TreeNode* temp = root;
-        while(temp->right != NULL){
-            temp = temp->right;
-        }
-        temp->right = r;
+        TreeNode* curr = root;
+        while(curr != NULL) {
+            if(curr->left != NULL) {
+                // predecessor = left subtree ka rightmost node
+                TreeNode* pred = curr->left;
+                while(pred->right != NULL) {
+                    pred = pred->right;
+                }
+                // original right subtree ko predecessor ke right me jod do
+                pred->right = curr->right;
+                // left subtree ko right me shift karo
+                curr->right = curr->left;
+                curr->left = NULL;
+            }
+            // next node
+            curr = curr->right;
+    }
     }
 };
