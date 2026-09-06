d096add26380705fe53341f7f5d5d7d253095f24
Rishabh patel <rishabh24464@iiitd.ac.in>
2026-09-06 21:15:11 +0530

    Time: 30 ms (72.3%), Space: 16.7 MB (46.41%) - LeetHub

---
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+        int ans = 0;
+
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0;
+            int j = k - 1;
+
+            while (i < j) {
+                if (nums[i] + nums[j] > nums[k]) {
+                    // All elements from i to j-1
+                    // can form a triangle with j and k
+                    ans += j - i;
+                    j--;
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
