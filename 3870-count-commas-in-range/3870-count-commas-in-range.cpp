3d5cb5f4e9fb8c77ca21f1c682ef437f2a582ca0
Rishabh patel <rishabh24464@iiitd.ac.in>
2026-09-08 11:13:23 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.47%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     long long countCommas(int n) {
-        return max(n-999,0);
+        long long ans = 0;
+
+        ans += max(n - 999, 0);
+        ans += max(n - 999999, 0);
+        ans += max(n - 999999999, 0);
+
+        return ans;
     }
 };
